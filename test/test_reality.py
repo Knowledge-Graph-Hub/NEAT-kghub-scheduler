bad822e6d8b09e9733a059ab228275163f86c6de
Justin Reese <justaddcoffee@gmail.com>
2022-02-01 10:30:01 -0800

    Add trivial test

---
+++ test/test_reality.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestReality(TestCase):
+    """Test reality
+    """
+    def setUp(self) -> None:
+        pass
+
+    def test_reality(self) -> None:
+        self.assertTrue(2+2==4)
+
